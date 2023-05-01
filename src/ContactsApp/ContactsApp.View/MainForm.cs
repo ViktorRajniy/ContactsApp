4b1f52f1b7fe87520a2221cdc2a1b5d04ed99d86
ViktorRajniy <83911994+ViktorRajniy@users.noreply.github.com>
2023-05-01 23:22:14 +0700

    ui(ContactForm, AboutForm): Добавлены формы и завершена их вёрстка

--- src/ContactsApp/ContactsApp.View/MainForm.cs
+++ src/ContactsApp/ContactsApp.View/MainForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ContactsApp.View
@@ -15,6 +9,73 @@ namespace ContactsApp.View
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private void RemoveContactButton_MouseLeave(object sender, EventArgs e)
+		{
+			RemoveContactButton.Image = Properties.Resources.remove_contact_32x32_gray;
+			RemoveContactButton.BackColor = Color.White;
+		}
+
+		private void RemoveContactButton_MouseEnter(object sender, EventArgs e)
+		{
+			RemoveContactButton.Image = Properties.Resources.remove_contact_32x32;
+			RemoveContactButton.BackColor = ColorTranslator.FromHtml("#FAF5F5");
+		}
+
+		private void EditContactButton_MouseEnter(object sender, EventArgs e)
+		{
+			EditContactButton.Image = Properties.Resources.edit_contact_32x32;
+			EditContactButton.BackColor = ColorTranslator.FromHtml("#F5F5FF");
+		}
+
+		private void EditContactButton_MouseLeave(object sender, EventArgs e)
+		{
+			EditContactButton.Image = Properties.Resources.edit_contact_32x32_gray;
+			EditContactButton.BackColor = Color.White;
+		}
+
+		private void AddContactButton_MouseEnter(object sender, EventArgs e)
+		{
+			AddContactButton.Image = Properties.Resources.add_contact_32x32;
+			AddContactButton.BackColor = ColorTranslator.FromHtml("#F5F5FF");
+		}
+
+		private void AddContactButton_MouseLeave(object sender, EventArgs e)
+		{
+			AddContactButton.Image = Properties.Resources.add_contact_32x32_gray;
+			AddContactButton.BackColor = Color.White;
+		}
+
+		private void AddContactButton_Click(object sender, EventArgs e)
+		{
+			var contactForm = new ContactForm();
+			contactForm.ShowDialog();
+		}
+
+		private void emailTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			e.Handled = true;
+		}
+
+		private void fullNameTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			e.Handled = true;
+		}
+
+		private void MainForm_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.F1)
+			{
+				var aboutForm = new AboutForm();
+				aboutForm.ShowDialog();
+			}
+		}
+
+		private void EditContactButton_Click(object sender, EventArgs e)
+		{
+			var contactForm = new ContactForm();
+			contactForm.ShowDialog();
 		}
 	}
 }
