0a8a1ac23bdb6393e03f11e8d3e9f7ccd38c21c8
ViktorRajniy <83911994+ViktorRajniy@users.noreply.github.com>
2023-05-11 22:15:24 +0700

    bug

---
+++ src/ContactsApp/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using ContactsApp.Model;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1;
+
+public class Program
+{
+	public static void Main()
+	{
+		Project test = new Project();
+		Console.WriteLine("BEFORE");
+		test.Contacts.Add(new Contact("a1", "someadress@mail.ru", "+0(000)-000-00-00", DateTime.Today, "vkid"));
+		test.Contacts.Add(new Contact("d4", "someadress@mail.ru", "+0(000)-000-00-00", DateTime.Today, "vkid"));
+		test.Contacts.Add(new Contact("c3", "someadress@mail.ru", "+0(000)-000-00-00", DateTime.Today, "vkid"));
+		test.Contacts.Add(new Contact("b2", "someadress@mail.ru", "+0(000)-000-00-00", DateTime.Today, "vkid"));
+
+		foreach(Contact contact in test.Contacts)
+		{
+			Console.WriteLine(contact.FullName);
+		}
+
+		Console.WriteLine("");
+		Console.WriteLine("AFTER");
+		test.Contacts = test.SortContactsByFullName(test.Contacts);
+		foreach (Contact contact in test.Contacts)
+		{
+			Console.WriteLine(contact.FullName);
+		}
+	}
+}
