b745ddc06f05dd091d11647a12e466e99366aac8
ViktorRajniy <83911994+ViktorRajniy@users.noreply.github.com>
2023-05-18 23:00:28 +0700

    ui(AboutForm): Переименованы все поля по стандарту

--- src/ContactsApp/ContactsApp.View/AboutForm.Designer.cs
+++ src/ContactsApp/ContactsApp.View/AboutForm.Designer.cs
@@ -29,143 +29,143 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.label6 = new System.Windows.Forms.Label();
-			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-			this.label8 = new System.Windows.Forms.Label();
-			this.label9 = new System.Windows.Forms.Label();
-			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.ContactsAppLable = new System.Windows.Forms.Label();
+			this.VersionLable = new System.Windows.Forms.Label();
+			this.AuthorLable = new System.Windows.Forms.Label();
+			this.EmailLable = new System.Windows.Forms.Label();
+			this.GithubLable = new System.Windows.Forms.Label();
+			this.LicenseTextBox = new System.Windows.Forms.TextBox();
+			this.LinksLable = new System.Windows.Forms.Label();
+			this.Icons8Lable = new System.Windows.Forms.LinkLabel();
+			this.EmailValueLable = new System.Windows.Forms.Label();
+			this.AuthorNameLable = new System.Windows.Forms.Label();
+			this.GithubLinkLable = new System.Windows.Forms.LinkLabel();
 			this.OkButton = new System.Windows.Forms.Button();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel1.SuspendLayout();
+			this.BottomPanel = new System.Windows.Forms.Panel();
+			this.BottomPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// label1
+			// ContactsAppLable
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(184, 32);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "ContactsApp";
+			this.ContactsAppLable.AutoSize = true;
+			this.ContactsAppLable.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.ContactsAppLable.Location = new System.Drawing.Point(12, 9);
+			this.ContactsAppLable.Name = "ContactsAppLable";
+			this.ContactsAppLable.Size = new System.Drawing.Size(184, 32);
+			this.ContactsAppLable.TabIndex = 0;
+			this.ContactsAppLable.Text = "ContactsApp";
 			// 
-			// label2
+			// VersionLable
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label2.Location = new System.Drawing.Point(15, 51);
-			this.label2.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(32, 15);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "v 1.0";
+			this.VersionLable.AutoSize = true;
+			this.VersionLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.VersionLable.Location = new System.Drawing.Point(15, 51);
+			this.VersionLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.VersionLable.Name = "VersionLable";
+			this.VersionLable.Size = new System.Drawing.Size(32, 15);
+			this.VersionLable.TabIndex = 1;
+			this.VersionLable.Text = "v 1.0";
 			// 
-			// label3
+			// AuthorLable
 			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label3.Location = new System.Drawing.Point(15, 76);
-			this.label3.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(45, 15);
-			this.label3.TabIndex = 2;
-			this.label3.Text = "Author:";
+			this.AuthorLable.AutoSize = true;
+			this.AuthorLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.AuthorLable.Location = new System.Drawing.Point(15, 76);
+			this.AuthorLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.AuthorLable.Name = "AuthorLable";
+			this.AuthorLable.Size = new System.Drawing.Size(45, 15);
+			this.AuthorLable.TabIndex = 2;
+			this.AuthorLable.Text = "Author:";
 			// 
-			// label4
+			// EmailLable
 			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label4.Location = new System.Drawing.Point(15, 101);
-			this.label4.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(46, 15);
-			this.label4.TabIndex = 3;
-			this.label4.Text = "E-mail:";
+			this.EmailLable.AutoSize = true;
+			this.EmailLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.EmailLable.Location = new System.Drawing.Point(15, 101);
+			this.EmailLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.EmailLable.Name = "EmailLable";
+			this.EmailLable.Size = new System.Drawing.Size(46, 15);
+			this.EmailLable.TabIndex = 3;
+			this.EmailLable.Text = "E-mail:";
 			// 
-			// label5
+			// GithubLable
 			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label5.Location = new System.Drawing.Point(15, 126);
-			this.label5.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(46, 15);
-			this.label5.TabIndex = 4;
-			this.label5.Text = "Github:";
+			this.GithubLable.AutoSize = true;
+			this.GithubLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.GithubLable.Location = new System.Drawing.Point(15, 126);
+			this.GithubLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.GithubLable.Name = "GithubLable";
+			this.GithubLable.Size = new System.Drawing.Size(46, 15);
+			this.GithubLable.TabIndex = 4;
+			this.GithubLable.Text = "Github:";
 			// 
-			// textBox1
+			// LicenseTextBox
 			// 
-			this.textBox1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBox1.Location = new System.Drawing.Point(18, 151);
-			this.textBox1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.textBox1.Size = new System.Drawing.Size(504, 139);
-			this.textBox1.TabIndex = 5;
-			this.textBox1.Text = resources.GetString("textBox1.Text");
-			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			this.LicenseTextBox.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.LicenseTextBox.Location = new System.Drawing.Point(18, 151);
+			this.LicenseTextBox.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+			this.LicenseTextBox.Multiline = true;
+			this.LicenseTextBox.Name = "LicenseTextBox";
+			this.LicenseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.LicenseTextBox.Size = new System.Drawing.Size(504, 139);
+			this.LicenseTextBox.TabIndex = 5;
+			this.LicenseTextBox.Text = resources.GetString("LicenseTextBox.Text");
+			this.LicenseTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
 			// 
-			// label6
+			// LinksLable
 			// 
-			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label6.Location = new System.Drawing.Point(15, 303);
-			this.label6.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(200, 15);
-			this.label6.TabIndex = 6;
-			this.label6.Text = "All used images are download from";
+			this.LinksLable.AutoSize = true;
+			this.LinksLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.LinksLable.Location = new System.Drawing.Point(15, 303);
+			this.LinksLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.LinksLable.Name = "LinksLable";
+			this.LinksLable.Size = new System.Drawing.Size(200, 15);
+			this.LinksLable.TabIndex = 6;
+			this.LinksLable.Text = "All used images are download from";
 			// 
-			// linkLabel1
+			// Icons8Lable
 			// 
-			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(221, 305);
-			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(61, 13);
-			this.linkLabel1.TabIndex = 7;
-			this.linkLabel1.TabStop = true;
-			this.linkLabel1.Text = "icons8.com";
-			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			this.Icons8Lable.AutoSize = true;
+			this.Icons8Lable.Location = new System.Drawing.Point(221, 305);
+			this.Icons8Lable.Name = "Icons8Lable";
+			this.Icons8Lable.Size = new System.Drawing.Size(61, 13);
+			this.Icons8Lable.TabIndex = 7;
+			this.Icons8Lable.TabStop = true;
+			this.Icons8Lable.Text = "icons8.com";
+			this.Icons8Lable.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
 			// 
-			// label8
+			// EmailValueLable
 			// 
-			this.label8.AutoSize = true;
-			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label8.Location = new System.Drawing.Point(81, 101);
-			this.label8.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(129, 15);
-			this.label8.TabIndex = 9;
-			this.label8.Text = "rajniyviktor@yandex.ru";
+			this.EmailValueLable.AutoSize = true;
+			this.EmailValueLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.EmailValueLable.Location = new System.Drawing.Point(81, 101);
+			this.EmailValueLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.EmailValueLable.Name = "EmailValueLable";
+			this.EmailValueLable.Size = new System.Drawing.Size(129, 15);
+			this.EmailValueLable.TabIndex = 9;
+			this.EmailValueLable.Text = "rajniyviktor@yandex.ru";
 			// 
-			// label9
+			// AuthorNameLable
 			// 
-			this.label9.AutoSize = true;
-			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label9.Location = new System.Drawing.Point(81, 76);
-			this.label9.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(86, 15);
-			this.label9.TabIndex = 8;
-			this.label9.Text = "Viktor Oxingert";
+			this.AuthorNameLable.AutoSize = true;
+			this.AuthorNameLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.AuthorNameLable.Location = new System.Drawing.Point(81, 76);
+			this.AuthorNameLable.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+			this.AuthorNameLable.Name = "AuthorNameLable";
+			this.AuthorNameLable.Size = new System.Drawing.Size(86, 15);
+			this.AuthorNameLable.TabIndex = 8;
+			this.AuthorNameLable.Text = "Viktor Oxingert";
 			// 
-			// linkLabel2
+			// GithubLinkLable
 			// 
-			this.linkLabel2.AutoSize = true;
-			this.linkLabel2.Location = new System.Drawing.Point(84, 126);
-			this.linkLabel2.Name = "linkLabel2";
-			this.linkLabel2.Size = new System.Drawing.Size(156, 13);
-			this.linkLabel2.TabIndex = 10;
-			this.linkLabel2.TabStop = true;
-			this.linkLabel2.Text = "https://github.com/ViktorRajniy";
-			this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+			this.GithubLinkLable.AutoSize = true;
+			this.GithubLinkLable.Location = new System.Drawing.Point(84, 126);
+			this.GithubLinkLable.Name = "GithubLinkLable";
+			this.GithubLinkLable.Size = new System.Drawing.Size(156, 13);
+			this.GithubLinkLable.TabIndex = 10;
+			this.GithubLinkLable.TabStop = true;
+			this.GithubLinkLable.Text = "https://github.com/ViktorRajniy";
+			this.GithubLinkLable.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
 			// 
 			// OkButton
 			// 
@@ -173,19 +173,19 @@
 			this.OkButton.Name = "OkButton";
 			this.OkButton.Size = new System.Drawing.Size(75, 23);
 			this.OkButton.TabIndex = 0;
-			this.OkButton.Text = "Ok";
+			this.OkButton.Text = "OK";
 			this.OkButton.UseVisualStyleBackColor = true;
 			this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
 			// 
-			// panel1
+			// BottomPanel
 			// 
-			this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
-			this.panel1.Controls.Add(this.OkButton);
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel1.Location = new System.Drawing.Point(0, 361);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(534, 50);
-			this.panel1.TabIndex = 24;
+			this.BottomPanel.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.BottomPanel.Controls.Add(this.OkButton);
+			this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.BottomPanel.Location = new System.Drawing.Point(0, 361);
+			this.BottomPanel.Name = "BottomPanel";
+			this.BottomPanel.Size = new System.Drawing.Size(534, 50);
+			this.BottomPanel.TabIndex = 24;
 			// 
 			// AboutForm
 			// 
@@ -194,24 +194,24 @@
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.BackColor = System.Drawing.Color.White;
 			this.ClientSize = new System.Drawing.Size(534, 411);
-			this.Controls.Add(this.panel1);
-			this.Controls.Add(this.linkLabel2);
-			this.Controls.Add(this.label8);
-			this.Controls.Add(this.label9);
-			this.Controls.Add(this.linkLabel1);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.BottomPanel);
+			this.Controls.Add(this.GithubLinkLable);
+			this.Controls.Add(this.EmailValueLable);
+			this.Controls.Add(this.AuthorNameLable);
+			this.Controls.Add(this.Icons8Lable);
+			this.Controls.Add(this.LinksLable);
+			this.Controls.Add(this.LicenseTextBox);
+			this.Controls.Add(this.GithubLable);
+			this.Controls.Add(this.EmailLable);
+			this.Controls.Add(this.AuthorLable);
+			this.Controls.Add(this.VersionLable);
+			this.Controls.Add(this.ContactsAppLable);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
 			this.Name = "AboutForm";
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
-			this.panel1.ResumeLayout(false);
+			this.BottomPanel.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -219,18 +219,18 @@
 
 		#endregion
 
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.LinkLabel linkLabel1;
-		private System.Windows.Forms.Label label8;
-		private System.Windows.Forms.Label label9;
-		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.Label ContactsAppLable;
+		private System.Windows.Forms.Label VersionLable;
+		private System.Windows.Forms.Label AuthorLable;
+		private System.Windows.Forms.Label EmailLable;
+		private System.Windows.Forms.Label GithubLable;
+		private System.Windows.Forms.TextBox LicenseTextBox;
+		private System.Windows.Forms.Label LinksLable;
+		private System.Windows.Forms.LinkLabel Icons8Lable;
+		private System.Windows.Forms.Label EmailValueLable;
+		private System.Windows.Forms.Label AuthorNameLable;
+		private System.Windows.Forms.LinkLabel GithubLinkLable;
 		private System.Windows.Forms.Button OkButton;
-		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel BottomPanel;
 	}
 }
