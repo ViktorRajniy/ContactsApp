9f0b79f55fa6c48cf7e2cd7cbf2fc093003acfba
ViktorRajniy <83911994+ViktorRajniy@users.noreply.github.com>
2023-06-03 03:56:32 +0700

    features(ProjectManager): реализованы методы сохранения и загрузки данных

--- src/ContactsApp/ContactsApp.Model/ProjectManager.cs
+++ src/ContactsApp/ContactsApp.Model/ProjectManager.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using static System.Environment;
 
 namespace ContactsApp.Model
 {
-	internal class ProjectManager
+	public static class ProjectManager
 	{
+		static string Filename { get; set; } = 
+			Environment.GetFolderPath(SpecialFolder.ApplicationData) + 
+			"\\Oxingert\\ContactsApp\\userdata.json";
+		static void SaveToFile(Project project)
+		{
+			//Создаём экземпляр сериализатора
+			JsonSerializer serializer = new JsonSerializer();
+
+			//Если файла не существует
+			if (!File.Exists(Filename))
+			{
+				DirectoryInfo directory = Directory.CreateDirectory(Filename);
+			}
+
+			//Открываем поток для записи в файл с указанием пути
+			StreamWriter sw = new StreamWriter(@Filename);
+			JsonWriter writer = new JsonTextWriter(sw);
+
+			//Вызываем сериализацию и передаем объект, который хотим сериализовать
+			serializer.Serialize(writer, project);
+
+		}
+
+		static Project LoadFromFile()
+		{
+			//Создаём переменную, в которую поместим результат десериализации
+			Project project = null;
+
+			//Если файла не существует
+			if (!File.Exists(Filename))
+			{
+				DirectoryInfo directory = Directory.CreateDirectory(Filename);
+				return project;
+			}
+
+			//Создаём экземпляр сериализатора
+			JsonSerializer serializer = new JsonSerializer();
+
+			//Открываем поток для чтения из файла с указанием пути
+			StreamReader sr = new StreamReader(@Filename);
+			JsonReader reader = new JsonTextReader(sr);
+			return project = (Project)serializer.Deserialize(reader);
+		}
+
 	}
 }
